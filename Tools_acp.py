d077a5deb91a73cf8ffbc2c1bfcde823ee79f78a
wky <wky@qq.com>
2024-05-13 13:30:55 +0800

    修改：Tools_acp.py{新增一个选择文件commit的功能}

--- Tools_acp.py
+++ Tools_acp.py
@@ -1,10 +1,10 @@
 import os
 
 
-def main(message):
+def main(message, modify_file):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     print("git add".center(50, "-"))
-    os.system("git add .")
+    os.system("git add " + modify_file)
     print("git commit".center(50, "-"))
     os.system("git commit -m %s" % (message,))
     print("git push".center(50, "-"))
@@ -16,10 +16,11 @@ def main(message):
 
 
 if __name__ == '__main__':
+    modify_file = input("Enter Modified File Path:\n").strip().strip('"')
     mess = input('输入提交信息>>>').strip().replace(' ', '-').replace('|', '-').replace('>', '-')
     decide = input("是否提交(y/n)>>>").strip().lower()
     if decide == 'y':
-        main(message=mess)
+        main(message=mess, modify_file=modify_file)
     else:
         print("未提交".center(50, "-"))
         os.system('pause')
